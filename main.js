b36567d7c8a28019dbaf368948977fd8b4be2ef7
Tyler Natsuhara <ty@ty.pizza>
2023-03-19 16:03:20 -0700

    add linux build and autformatting

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-const promisify = require("util").promisify;
-const nativefier = promisify(require("nativefier").default);
-const fs = require("fs");
-const archiver = require("archiver");
-const process = require("process");
+const promisify = require("util").promisify
+const nativefier = promisify(require("nativefier").default)
+const fs = require("fs")
+const archiver = require("archiver")
+const process = require("process")
 
 const zip = (platform) => {
-  const builtDirectory = `build/SWAMP CAMP-${platform}`;
-  const output = fs.createWriteStream(
-    `${__dirname}/build/swamp-camp-${platform}.zip`
-  );
-  const archive = archiver("zip", {
-    zlib: { level: 9 }, // Sets the compression level
-  });
-  archive.directory(builtDirectory, false);
-  archive.pipe(output);
-  archive.finalize();
-};
+    const builtDirectory = `build/SWAMP CAMP-${platform}`
+    const output = fs.createWriteStream(`${__dirname}/build/swamp-camp-${platform}.zip`)
+    const archive = archiver("zip", {
+        zlib: { level: 9 }, // Sets the compression level
+    })
+    archive.directory(builtDirectory, false)
+    archive.pipe(output)
+    archive.finalize()
+}
 
-const options = {
-  name: "SWAMP CAMP",
-  targetUrl: "https://swamp.camp?native_app=true",
-  out: "build/",
-  overwrite: true,
-  icon: process.platform === "win32" ? "icon.ico" : "icon.png",
-  width: 1280,
-  height: 800,
-  showMenuBar: false,
-  fastQuit: false,
-  maximize: true,
-};
+const main = async () => {
+    await nativefier({
+        name: "SWAMP CAMP",
+        targetUrl: "https://swamp.camp?native_app=true",
+        out: "build/",
+        overwrite: true,
+        icon: process.platform === "win32" ? "icon.ico" : "icon.png",
+        width: 1280,
+        height: 800,
+        showMenuBar: false,
+        fastQuit: false,
+        maximize: true,
+    })
 
-nativefier(options).then(() => zip(`${process.platform}-${process.arch}`));
+    zip(`${process.platform}-${process.arch}`)
+}
+
+main()
